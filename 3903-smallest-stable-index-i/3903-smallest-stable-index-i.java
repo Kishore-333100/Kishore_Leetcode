8bb26fc0fd70643e778f98d4b4ec4d909a47d176
Kishore G <elegajendran@gmail.com>
2026-09-04 15:44:42 +0530

    Time: 1 ms (99.79%), Space: 46.1 MB (94.47%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -4,7 +4,7 @@ class Solution {
         int [] min = new int[n];
         min[n-1] = nums[n-1];
         for(int i=n-2;i>=0;i--) min[i]=Math.min(min[i+1],nums[i]);
-        int max = nums[0];
+        int max = 0;
         for(int i=0;i<n;i++){
             max=Math.max(max,nums[i]);
             if(max-min[i]<=k) return i;
